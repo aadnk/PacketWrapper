8836f811c75bcea037bcbf992ef103113e54abd2
Kristian S. Stangeland <kr_stang@hotmail.com>
2013-05-15 06:28:52 +0200

    Adding additional status values.

--- PacketWrapper/src/main/java/com/comphenix/packetwrapper/Packet26EntityStatus.java
+++ PacketWrapper/src/main/java/com/comphenix/packetwrapper/Packet26EntityStatus.java
@@ -34,6 +34,13 @@ public class Packet26EntityStatus extends AbstractPacket {
     	public static final int WOLF_SHAKING_OFF_WATER = 8;
     	public static final int EATING_ACCEPTED = 9;
     	public static final int SHEEP_EATING_GRASS = 10;
+    	public static final int IRON_GOLEM_GIFTING_ROSE = 11;
+    	public static final int VILLAGER_SPAWN_HEART_PARTICLE = 12;
+    	public static final int VILLAGER_SPAWN_ANGRY_PARTICLE = 13;
+    	public static final int VILLAGER_SPAWN_HAPPY_PARTICLE = 14;
+    	public static final int WITCH_SPAWN_MAGIC_PARTICLE = 15;
+    	public static final int ZOMBIE_VILLAGERIZING = 16;
+    	public static final int FIREWORK_EXPLODING = 17;
     	
 		/**
 		 * The singleton instance. Can also be retrieved from the parent class.
